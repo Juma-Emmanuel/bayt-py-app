f91871fba6db33fc93aced55d5df0abf923fecf6
Juma-Emmanuel <jumaemmanuel462@yahoo.com>
2024-01-21 18:48:00 +0300

    updates domain

--- bayt_api/settings.py
+++ bayt_api/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'baytadmin.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'baytadmin2024.pythonanywhere.com']
 ALLOWED_HOSTS  += os.environ.get("ALLOWED_HOSTS").split()
 
 # Application definition
